95243369f410228cac5ee89d0f90e6d9d981c87b
sve1r <plekiyeon@gmail.com>
2021-04-04 21:26:27 +0800

    微信去广告

---
+++ Scripts/AdBlock/Wechat/Wechat.js
@@ -0,0 +1,15 @@
+﻿/*
+Remove the WeChat public account bottom ad
+by Choler
+QX:
+^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+Surge4：
+http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+Surge & QX MITM = mp.weixin.qq.com,
+*/
+
+var obj = JSON.parse($response.body);
+obj.advertisement_num = 0;
+obj.advertisement_info = [];
+delete obj.appid;
+$done({body: JSON.stringify(obj)}); 
