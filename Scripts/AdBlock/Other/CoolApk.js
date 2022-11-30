415cb83be04c617b0cdfb8e994e046fbf763fca8
sve1r <plekiyeon@gmail.com>
2022-11-30 17:11:50 +0800

    ✨ feat: 随原版本更新脚本（powered by @ddgksf2013）

--- Scripts/AdBlock/Other/CoolApk.js
+++ Scripts/AdBlock/Other/CoolApk.js
@@ -7,7 +7,7 @@ author      kk pp
 
 
 [rewrite_local]
-^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://ocd0522.tk/ddgksf2013/Cuttlefish/raw/branch/master/Script/coolapk.js
+^https?:\/\/api.coolapk.com\/v6\/(feed\/(replyList|detail)|main\/indexV8|dataList) url script-response-body https://github.com/ddgksf2013/Scripts/raw/master/coolapk.js
 
 [mitm]
 hostname = api.coolapk.com
--- Scripts/AdBlock/Other/Keep.js
+++ Scripts/AdBlock/Other/Keep.js
@@ -1,19 +1,9 @@
-
-
 if (-1 != $request.url.indexOf("athena/v5/people/my")) {
     let e = JSON.parse($response.body);
     e.data.floatingInfo = {}, $done({body: JSON.stringify(e)})
 } else if (-1 != $request.url.indexOf("config/v3/basic")) {
     let e = JSON.parse($response.body);
-    e.data.bottomBarControl.defaultTab = "home",
-        e.data.bottomBarControl.tabs = Object.values(e.data.bottomBarControl.tabs)
-        .filter(e => !("entry" == e.tabType || "mall" == e.tabType || "prime" == e.tabType)),
-        e.data.homeTabs = Object.values(e.data.homeTabs)
-            .filter(e => !("uni_web_activity" == e.type)), 2 < e.data.homeTabs.length && (e.data.homeTabs[0].schema = "keep://homepage/homePrime",
-        e.data.homeTabs[0].name = "會員",
-        e.data.homeTabs[0].type = "homePrime"),
-        $done({body: JSON.stringify(e)}
-        )
+    e.data.bottomBarControl.defaultTab = "home", e.data.bottomBarControl.tabs = Object.values(e.data.bottomBarControl.tabs).filter(e => !("entry" == e.tabType || "mall" == e.tabType || "prime" == e.tabType)), e.data.homeTabs = Object.values(e.data.homeTabs).filter(e => !("uni_web_activity" == e.type)), 2 < e.data.homeTabs.length && (e.data.homeTabs[0].schema = "keep://homepage/homePrime", e.data.homeTabs[0].name = "會員", e.data.homeTabs[0].type = "homePrime"), $done({body: JSON.stringify(e)})
 } else if (-1 != $request.url.indexOf("homepage/v7/tab/find")) {
     let e = JSON.parse($response.body);
     e.data.sections = Object.values(e.data.sections).filter(e => "quickEntranceV3" == e.contentStyle), e.data.sections[0].quickEntrances = e.data.sections[0].quickEntrances.filter(e => 0 == e.itemTrackProps.itemPosition || 1 == e.itemTrackProps.itemPosition || 2 == e.itemTrackProps.itemPosition || 3 == e.itemTrackProps.itemPosition), $done({body: JSON.stringify(e)})
