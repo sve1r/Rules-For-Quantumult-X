72d489ba867a451353e4e75eca0f481845878c74
sve1r <plekiyeon@gmail.com>
2023-02-01 16:11:57 +0800

    fix: 🛸 优化去广告相关

--- Scripts/AdBlock/JD/jx_startup.js
+++ Scripts/AdBlock/JD/jx_startup.js
@@ -1,6 +1,11 @@
-if (/^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test($request.url)) {
-    let obj=JSON.parse($response.body);
-    obj.data.materialList.startTime=3667476800000;
-    obj.data.materialList.endTime  =3667908800000;
-    $done({body:JSON.stringify(obj)});
+let obj = JSON.parse($response.body);
+/*京喜APP*/
+if (
+  /^https?:\/\/api\.m\.jd\.com\/api\?functionId=delivery_show/.test(
+    $request.url
+  )
+) {
+  obj.data.materialList.startTime = 3667476800000;
+  obj.data.materialList.endTime = 3667908800000;
 }
+$done({ body: JSON.stringify(obj) });
