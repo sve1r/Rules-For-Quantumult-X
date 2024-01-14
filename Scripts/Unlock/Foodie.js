ef66831a66b64d2a4d9504b6d3184e1add87ba4b
SVE1R <plekiyeon@gmail.com>
2024-01-14 19:47:45 +0800

    🍜 feat： 新增 Foodie 解锁

---
+++ Scripts/Unlock/Foodie.js
@@ -0,0 +1,16 @@
+
+let objc =  {
+    "result" : {
+        "activated" : true,
+        "products" : [
+            {
+                "managed" : false,
+                "status" : "ACTIVE",
+                "startDate" : 1666666666666,
+                "productId" : "com.linecorp.Foodiecn.subscribe.oneyear",
+                "expireDate" : 4092599349000
+            }
+        ]
+    }
+}
+$done({body : JSON.stringify(objc)});
